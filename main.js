3ba966ba7cd5a2ec8ae6d609da4fd2a425125543
CodingWolfGirl303 <92897209+CodingWolfGirl303@users.noreply.github.com>
2021-12-18 21:30:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var i_like = "I like ";
+console.log(i_like);
+var item = "Horses";
+console.log(item);
+var i_like_item = i_like.concat(item);
+console.log(i_like_item);
+var number1 = 10;
+console.log(number1);
+var number2 = 6;
+console.log(number2);
+var addition = number1 + number2
+console.log(addition);
+var subtraction = number1 - number2
+console.log(subtraction);
+var multiplication = number1 * number2
+console.log(multiplication);
+var division = number1 / number2
+console.log(division);
